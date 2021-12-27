2bcd21118009cc0e3ed0e74653448bd0d60e4579
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-27 14:09:22 +0200

    expose layout, added success and error pages

---
+++ pages/success.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Success = () => {
+    return (
+        <Layout>
+            success
+        </Layout>
+    );
+};
+
+export default Success;
